4c087f8e0140fb290dbf5fd55c5fe26c2d80030a
Galeyee <galwang173@gmail.com>
2023-07-11 11:41:19 +0600

    assignment-1

---
+++ angular-udemy/src/app/server/server.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-server',
+    // selector: '[app-server]', //attribute selector
+    // selector: '.app-server', //for reference to select by class use .
+    templateUrl: './server.component.html'
+})
+
+export class ServerComponent {
+
+}
